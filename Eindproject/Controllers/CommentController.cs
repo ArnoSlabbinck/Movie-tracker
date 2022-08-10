3ffe5efaa2f2fd29757f7f16c262098ecac9d751
Arno Slabbinck <arno.slabbinck@hotmail.com>
2022-08-11 00:32:32 +0200

    Add project files.

---
+++ Eindproject/Controllers/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using Eindproject.Data;
+using Eindproject.Domain;
+using Eindproject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eindproject.Controllers
+{
+
+    public class CommentController : Controller
+    {
+        private ICommentRepository commentRepository;
+        public CommentController(ICommentRepository commentRepository)
+        {
+            this.commentRepository = commentRepository;
+         
+        }
+
+
+        public IActionResult DeleteComment(Comment comment)
+        {
+            commentRepository.DeleteComment(comment.CommentId);
+            return RedirectToAction("Movie/ViewFilmSerie");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(CommentViewModel comment)
+        {
+            var editComment = commentRepository.GetComment(comment.Comment_Id);
+            editComment.Comment_Message = comment.Comment_Message;
+            commentRepository.UpdateComment(editComment);
+            return RedirectToAction("Movie/ViewFilmSerie");
+        }
+        [HttpGet]
+        public IActionResult Edit(MovieCommentViewModel movieCommentViewModel)
+        {
+            CommentViewModel commentView = new CommentViewModel();
+            return PartialView("CommentModalPartial", commentView);
+
+        }
+
+
+      
+
+    }
+}
